9dd9c96b1c0c4327f186445e12a70486895764ed
philipwisner <philipwisner@gmail.com>
2017-09-13 23:15:14 +0200

    photo upload work

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -2,13 +2,17 @@ const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const multer  = require('multer');
+const User = require('../models/user');
+var upload = multer({ dest: './public/uploads/' });
+
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
     res.render('authentication/login', { message: req.flash('error')});
 });
 
 router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
-  successRedirect : '/',
+  successRedirect : '/profile',
   failureRedirect : '/login',
   failureFlash : true
 }));
@@ -18,7 +22,7 @@ router.get('/signup', ensureLoggedOut(), (req, res) => {
 });
 
 router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
-  successRedirect : '/',
+  successRedirect : '/profile',
   failureRedirect : '/signup',
   failureFlash : true
 }));
@@ -29,9 +33,26 @@ router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
     });
 });
 
-router.post('/logout', ensureLoggedIn('/login'), (req, res) => {
+router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
     req.logout();
     res.redirect('/');
 });
+
+
+router.post('/upload', upload.single('photo'), function(req, res){
+ const updates = { picture: {
+      name: req.body.name,
+      pic_path: `/uploads/${req.file.filename}`,
+      pic_name: req.file.originalname
+    }
+  }
+    User.findByIdAndUpdate(req.user._id, updates, (err, user) => {
+      res.redirect('/profile');
+  });
+  User.save((err) => {
+      res.redirect('/');
+  });
+});
+
 
 module.exports = router;
